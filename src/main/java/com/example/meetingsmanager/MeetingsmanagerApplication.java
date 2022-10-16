f6334d8b9af04e915c9a71436601b523f8194475
Mantas <mantas.ptakauskas@gmail.com>
2022-10-16 12:31:50 +0300

    initialise

---
+++ src/main/java/com/example/meetingsmanager/MeetingsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetingsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meetingsmanager/MeetingsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetingsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
